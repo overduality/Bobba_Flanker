d4de0bf8168aeaf74732e476ff4108c006a9926e
Evan Lokajaya <lokajaya414@gmail.com>
2025-03-26 11:01:38 +0700

    Remove unused protocol from User model

--- CoBo/Models/User.swift
+++ CoBo/Models/User.swift
@@ -9,11 +9,7 @@ import Foundation
 import SwiftData
 
 @Model
-class User: Codable {
-    enum keys: CodingKey {
-        case name
-        case email
-    }
+class User{
     
     var name: String
     var email: String
@@ -21,17 +17,5 @@ class User: Codable {
     init(name: String, email: String) {
         self.name = name
         self.email = email
-    }
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: keys.self)
-        self.name = try container.decode(String.self, forKey: .name)
-        self.email = try container.decode(String.self, forKey: .email)
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: keys.self)
-        try container.encode(name, forKey: .name)
-        try container.encode(email, forKey: .email)
     }
 }
