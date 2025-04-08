e2082218c6372cad726a9ad8730925ca967492de
Evan Lokajaya <lokajaya414@gmail.com>
2025-04-08 22:37:29 +0700

    Implement Auto Check In

--- CoBo/Views/BookingConfirmationView.swift
+++ CoBo/Views/BookingConfirmationView.swift
@@ -130,6 +130,7 @@ struct BookingConfirmationView: View {
         .navigationTitle("Booking Confirmation")
         .navigationBarTitleDisplayMode(.inline)
         .navigationDestination(for: BookingSuccessContext.self) { context in
+            // TODO: If the booking is auto checkin then it will show different success view. There is isAutoCheckIn in context
             BookingSuccessView(navigationPath: $navigationPath, booking: context.booking)
         }
         .onAppear() {
@@ -139,8 +140,74 @@ struct BookingConfirmationView: View {
     
     func confirmBooking() {
         guard let unwrappedBooking = booking else { return }
+        
+        let isAutoCheckIn = isAutoCheckInBooking(unwrappedBooking)
+        
+        if isAutoCheckIn {
+            unwrappedBooking.status = .checkedIn
+        }
+        
         bookingController.addBooking(booking: unwrappedBooking)
-        navigationPath.append(BookingSuccessContext(booking: unwrappedBooking))
+        navigationPath.append(BookingSuccessContext(booking: unwrappedBooking, isAutoCheckIn: isAutoCheckIn))
+    }
+    
+    func isAutoCheckInBooking(_ booking: Booking) -> Bool {
+        let startCheckIn = booking.timeslot.startCheckIn
+        let endCheckIn = booking.timeslot.endCheckIn
+        let endTimeslot = doubleToTime(booking.timeslot.endHour)
+        let date = booking.date
+        let calendar = Calendar.current
+        let now = Date()
+        
+        if !calendar.isDate(now, inSameDayAs: date) {
+            return false
+        }
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        
+        if let startTime = formatter.date(from: startCheckIn),
+           let endTime = formatter.date(from: endCheckIn), let endTimeslot = formatter.date(from: endTimeslot) {
+            
+            let startTimeComponents = calendar.dateComponents([.hour, .minute], from: startTime)
+            let endTimeComponents = calendar.dateComponents([.hour, .minute], from: endTime)
+            let endTimeslotTimeComponents = calendar.dateComponents([.hour, .minute], from: endTimeslot)
+            
+            var startDateComponents = calendar.dateComponents([.year, .month, .day], from: date)
+            startDateComponents.hour = startTimeComponents.hour
+            startDateComponents.minute = startTimeComponents.minute
+
+            var endDateComponents = calendar.dateComponents([.year, .month, .day], from: date)
+            endDateComponents.hour = endTimeComponents.hour
+            endDateComponents.minute = endTimeComponents.minute
+            
+            var endTimeslotComponents = calendar.dateComponents([.year, .month, .day], from: date)
+            endTimeslotComponents.hour = endTimeslotTimeComponents.hour
+            endTimeslotComponents.minute = endTimeslotTimeComponents.minute
+            
+            if let startDate = calendar.date(from: startDateComponents),
+               let endDate = calendar.date(from: endDateComponents),
+               let endTimeslotDate = calendar.date(from: endTimeslotComponents) {
+                if now >= startDate && now <= endDate {
+                    return true
+                }
+                else if now >= endDate && now <= endTimeslotDate {
+                    return true
+                }
+            }
+        }
+        
+        return false
+    }
+    
+    func doubleToTime(_ number: Double) -> String {
+        let hour = Int(number.rounded(.towardZero))
+        let minute = Int(number.truncatingRemainder(dividingBy: 1) * 60)
+        
+        let stringHour = hour < 10 ? "0\(hour)" : String(hour)
+        let stringMinute = minute < 10 ? "0\(minute)" : String(minute)
+        
+        return "\(stringHour):\(stringMinute)"
     }
 }
 
