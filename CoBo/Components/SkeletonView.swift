48725cc136ca266ce10999771b22025939d6ab37
overduality <emmanuelbobba3@gmail.com>
2025-04-19 18:02:16 +0700

    Skeleton View on Main Page and Booking Log

---
+++ CoBo/Components/SkeletonView.swift
@@ -0,0 +1,18 @@
+//
+//  SkeletonView.swift
+//  CoBo
+//
+//  Created by Rieno on 18/04/25.
+//
+
+import SwiftUI
+
+struct SkeletonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SkeletonView()
+}
